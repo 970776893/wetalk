2a62e33573ae505548a62b4a3dff58e65027e0a2
我叫张凯 <970776893@qq.com>
2016-05-02 13:02:01 -0400

    整理代码规范

--- dist/js/weTalk.js
+++ dist/js/weTalk.js
@@ -55,46 +55,46 @@ app.directive('weLayout', function () {
 app.directive("ngTouchstart", function () {
 	return {
 		controller: function ($scope, $element, $attrs) {
-			$element.bind('touchstart', onTouchStart);
 			function onTouchStart(event) {
 				var method = $element.attr('ng-touchstart');
 				$scope.$event = event;
 				$scope.$apply(method);
-			};
+			}
+			$element.bind('touchstart', onTouchStart);
 		}
 	};
 });
 app.directive("ngTouchmove", function () {
 	return {
 		controller: function ($scope, $element, $attrs) {
-			$element.bind('touchstart', onTouchStart);
 			function onTouchStart(event) {
 				event.preventDefault();
 				$element.bind('touchmove', onTouchMove);
 				$element.bind('touchend', onTouchEnd);
-			};
+			}
 			function onTouchMove(event) {
 				var method = $element.attr('ng-touchmove');
 				$scope.$event = event;
 				$scope.$apply(method);
-			};
+			}
 			function onTouchEnd(event) {
 				event.preventDefault();
 				$element.unbind('touchmove', onTouchMove);
 				$element.unbind('touchend', onTouchEnd);
-			};
+			}
+			$element.bind('touchstart', onTouchStart);
 		}
 	};
 });
 app.directive("ngTouchend", function () {
 	return {
 		controller: function ($scope, $element, $attrs) {
-			$element.bind('touchend', onTouchEnd);
 			function onTouchEnd(event) {
 				var method = $element.attr('ng-touchend');
 				$scope.$event = event;
 				$scope.$apply(method);
-			};
+			}
+			$element.bind('touchend', onTouchEnd);
 		}
 	};
 });
