cfb64af8523b39589c98e92f1fe0fdcccc5a7922
我叫张凯 <970776893@qq.com>
2016-05-08 03:19:25 -0400

    测试显示问题

--- app/modules/talk/talk_window_controller.js
+++ app/modules/talk/talk_window_controller.js
@@ -23,12 +23,63 @@ app.controller("talkWindowController", function ($rootScope, $scope, $location) 
 			id : 1,
 			content : '反正是很污反正是很污反正是很污反正是很污反正是很污反正是很污反正是很污反正是很污反正是很污',
 			isMine: 1
+		},{
+			id : 1,
+			content : '对呀对呀',
+			isMine: 1
+		},{
+			id : 1,
+			content : '反正是很污反正是很污反正是很污反正是很污反正是很污反正是很污反正是很污反正是很污反正是很污',
+			isMine: 1
+		},{
+			id : 1,
+			content : '有多污啊',
+			isMine: 0
+		},{
+			id : 1,
+			content : '反正是很污反正是很污反正是很污反正是很污反正是很污反正是很污反正是很污反正是很污反正是很污',
+			isMine: 1
+		},{
+			id : 1,
+			content : '对呀对呀',
+			isMine: 1
+		},{
+			id : 1,
+			content : '反正是很污反正是很污反正是很污反正是很污反正是很污反正是很污反正是很污反正是很污反正是很污',
+			isMine: 1
+		},{
+			id : 1,
+			content : '有多污啊',
+			isMine: 0
+		},{
+			id : 1,
+			content : '反正是很污反正是很污反正是很污反正是很污反正是很污反正是很污反正是很污反正是很污反正是很污',
+			isMine: 1
 		}
 	];
 
 	// 回车键发送消息
 	$scope.keySend = function($event, inputContent){
 		
+	};
+	//收取消息
+	$scope.getMsg = function(){
+		var msgContent = '测试受到消息';
+		//内容为空则不发送信息
+		if(!msgContent){
+			return ;
+		}
+		//正在发送，置灰发送按钮
+		$scope.sending = true;
+		$scope.historyList.push({
+			id : 1,
+			content : msgContent,
+			isMine : 0
+		});
+		//手动渲染
+		$scope.$apply();
+		//滚动条滚动到底部
+		$(document).scrollTop(100000000);
 	};
 	//发送消息
 	$scope.sendMsg = function(){
@@ -45,19 +96,23 @@ app.controller("talkWindowController", function ($rootScope, $scope, $location) 
 			isMine : 1
 		});
 		//手动渲染
-		$scope.$digest();
+		$scope.$apply();
 		//滚动条滚动到底部
-		//$('table').scrollTop(100000000);
+		$(document).scrollTop(100000000);
 		//清空输入框内容
 		$("#msgInput").val(null);
 		//获取焦点
-		//$("#msgInput").focus(); 
-		
+		//$("#msgInput").focus();
+		// TODO 测试收到回复 
+		$scope.getMsg();
 	};
-
+	//监听滚动事件
 	$(window).scroll(function(){
-		var s=$(window).scrollTop();
-		console.log(s);
+		var top =$(window).scrollTop();
+		if(top == 0){
+			// 滚动到顶部
+			$scope.showLoading = true;
+		}
 	});
 
 	$("#msgInput").bind('keyup', function(event){
@@ -68,5 +123,4 @@ app.controller("talkWindowController", function ($rootScope, $scope, $location) 
 	$("#msgBtn").bind('click', function(){
 		$scope.sendMsg();
 	});
-
 });
