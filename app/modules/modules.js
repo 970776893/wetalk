ad4557db213d6830ee506843a9ae5cc14aa80c9b
970776893 <39595931+970776893@users.noreply.github.com>
2023-01-29 17:39:44 +0800

    feet github page

--- app/modules/modules.js
+++ app/modules/modules.js
@@ -2,19 +2,19 @@ app.config( function ($routeProvider) {
 
 	$routeProvider
 	.when('/userList', {
-		templateUrl: "./app/modules/user/htmls/user_list.html",
+		templateUrl: "/wetalk/app/modules/user/htmls/user_list.html",
 		controller : "userListController"
 	})
 	.when('/talkList', {
-		templateUrl: "./app/modules/talk/htmls/talk_list.html",
+		templateUrl: "/wetalk/app/modules/talk/htmls/talk_list.html",
 		controller : "talkListController"
 	})
 	.when('/talkWindow/:id', {
-		templateUrl: "./app/modules/talk/htmls/talk_window.html",
+		templateUrl: "/wetalk/app/modules/talk/htmls/talk_window.html",
 		controller : "talkWindowController"
 	})
 	.otherwise({
-		templateUrl: "./app/modules/base/htmls/unknow.part.html",
+		templateUrl: "/wetalk/app/modules/base/htmls/unknow.part.html",
 		controller : function($rootScope, $scope){
 			$rootScope.bodyClass = 'error-page';
 		}
